2c8f78b86064b0984be9ca2601586cb49b955115
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-09-19 23:16:31 +0300

    🔧 Fix CORS error by setting correct headers

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
-
+app.use((req, res, next) => {
+  // Fix CORS error
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 app.use(bodyParser.json()); // parse application/json
 app.use("/feed", feedRoutes);
 
