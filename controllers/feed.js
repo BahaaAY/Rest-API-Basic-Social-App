2be88df155a57d4dac72b818e223690ad50ea272
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-09-20 20:23:02 +0300

    ✅ Added server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,44 @@
-const e = require("express");
-
+const { validationResult } = require("express-validator");
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        creator: {
+          name: "Bahaa",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  console.log("Creating Post!");
   // Create post in db
   const title = req.body.title;
   const content = req.body.content;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // Status code 422 means something went wrong with the validation
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
   // Status code 201 means something was created
-  res.status(201).json({
+  return res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "Bahaa",
+      },
+      createdAt: new Date(),
+    },
   });
 };
