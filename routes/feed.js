6e3d45e87cbf9a07280ddca6acbb17e2a357c61f
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-09-20 08:57:26 +0300

    📱 Frontend Setup!

--- routes/feed.js
+++ routes/feed.js
@@ -7,6 +7,7 @@ const feedController = require("../controllers/feed");
 //GET /feed/posts
 router.get("/posts", feedController.getPosts);
 
+//POST /feed/post
 router.post("/post", feedController.createPost);
 
 module.exports = router;
